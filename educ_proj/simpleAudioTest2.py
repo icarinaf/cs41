aa0886a1ce935a1909bf6c84c2cf0bd8c8b6395d
samkwok1 <106202444+samkwok1@users.noreply.github.com>
2022-05-30 18:28:16 -0700

    Add files via upload

--- educ_proj/simpleAudioTest2.py
+++ educ_proj/simpleAudioTest2.py
@@ -3,12 +3,6 @@ import simpleaudio as sa
 import matplotlib.image as mpimg
 from PIL import Image
 NOTES = {
-    "C0":16.35,
-    "E0":20.60,
-    "G0":24.50,
-    "C1":32.70,
-    "E1":41.20,
-    "G1":49.00,
     "C2":65.41,
     "E2":82.41,
     "G2":98.00,
@@ -47,42 +41,36 @@ colors = 'images/image.png'
 squidward = 'images/squidward.jpg'
 starry_night = 'images/starry_night.jpeg'
 
-image = Image.open(color_image, 'r')
-row_length, num_rows = image.size
-pix_val = list(image.getdata())
-#pix_val = [[0,0,0],[1,1,1],[2,2,2]]
-result = []
-rows = []
-#print(result)
-temp = 0
-for i in range(0, len(pix_val)):
-    if (i % row_length == 0):
-        val = int(temp / row_length)
-        rows.append(val)
-        temp = 0
-    else:
-        temp += np.average(pix_val[i])
+def findRows(image_name):
+    image = Image.open(image_name, 'r')
+    row_length, num_rows = image.size
+    pix_val = list(image.getdata())
+    #pix_val = [[0,0,0],[1,1,1],[2,2,2]]
+    result = []
+    rows = []
+    #print(result)
+    temp = 0
+    for i in range(0, len(pix_val)):
+        if (i % row_length == 0):
+            val = int(temp / row_length)
+            rows.append(val)
+            temp = 0
+        else:
+            temp += np.average(pix_val[i])
 
-temp = 0
-for i in range(0, len(rows)):
-    if (i % 9 == 0):
-        val = int(temp / 9)
-        result.append(val)
-        temp = 0
-    else:
-        temp += rows[i]
+    temp = 0
+    for i in range(0, len(rows)):
+        if (i % 3 == 0):
+            val = int(temp / 3)
+            result.append(val)
+            temp = 0
+        else:
+            temp += rows[i]
+    return result
 
 
 
-
-all_rows = result
-
-previous_val = -1
-curr_octave = 5
-row_index = 0
-up_or_down = 1
-
-def play_notes(NOTES, curr_octave):
+def play_notes_up(NOTES, curr_octave):
     C_freq = NOTES["C" + str(curr_octave)]
     E_freq = NOTES["E" + str(curr_octave)]
     G_freq = NOTES["G" + str(curr_octave)]
@@ -122,373 +110,95 @@ def play_notes(NOTES, curr_octave):
             #  wait for playback to finish before exiting
     play_obj.wait_done()
 
-while row_index < len(rows) - 1:
-    curr_val = rows[row_index]
 
-    # If the current row value is greater than the previous
-    # row value, go up!
-    if curr_val > previous_val:
+def play_notes_down(NOTES, curr_octave):
+    C_freq = NOTES["C" + str(curr_octave)]
+    E_freq = NOTES["E" + str(curr_octave)]
+    G_freq = NOTES["G" + str(curr_octave)]
 
-        # If last "move" was a down, ascend but in current octave
-        if up_or_down == -1:
-            play_notes(NOTES, curr_octave)
-            up_or_down = 1
+            # get timesteps for each sample, T is note duration in seconds
+    sample_rate = 44100
+    T = 0.75
+    t = np.linspace(0, T, int(T * sample_rate), False)
+
+            # generate sine wave notes
+    C_note = np.sin(C_freq * t * 2 * np.pi)
+    E_note = np.sin(E_freq * t * 2 * np.pi)
+    G_note = np.sin(G_freq * t * 2 * np.pi)
+
+            # mix audio together
+    audio = np.zeros((44100, 2))
+    n = len(t)
+    offset = 0
+    audio[0 + offset: n + offset, 0] += G_note
+    audio[0 + offset: n + offset, 1] += 0.125 * G_note
+            #offset = 0 to overlap notes completely
+    offset = 5500
+    audio[0 + offset: n + offset, 0] += 0.5 * E_note
+    audio[0 + offset: n + offset, 1] += 0.5 * E_note
+    offset = 11000
+    audio[0 + offset: n + offset, 0] += 0.125 * C_note
+    audio[0 + offset: n + offset, 1] += C_note
+
+            # normalize to 16-bit range
+    audio *= 32767 / np.max(np.abs(audio))
+            # convert to 16-bit data
+    audio = audio.astype(np.int16)
+
+            # start playback
+    play_obj = sa.play_buffer(audio, 2, 2, sample_rate)
+
+            #  wait for playback to finish before exiting
+    play_obj.wait_done()
+
+
+def playImage(image):
+    rows = findRows(image)
+    previous_val = -1
+    curr_octave = 5
+    row_index = 0
+    up_or_down = 1
+
+    while row_index < len(rows) - 1:
+        curr_val = rows[row_index]
+
+        # If the current row value is greater than the previous
+        # row value, go up!
+        if curr_val > previous_val:
+
+            # If last "move" was a down, ascend but in current octave
+            if up_or_down == -1:
+                play_notes_up(NOTES, curr_octave)
+                up_or_down = 1
             
-        # If last "move" was an up, ascend in next highest octave
-        if up_or_down == 1:
-            curr_octave += 1
-            play_notes(NOTES, curr_octave)
+            # If last "move" was an up, ascend in next highest octave
+            elif up_or_down == 1:
+                if curr_octave == 8:
+                    play_notes_up(NOTES, curr_octave)
+                else:
+                    curr_octave += 1
+                    play_notes_up(NOTES, curr_octave)
     
-    if curr_val < previous_val:
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-A_freq = 261.63
-Csh_freq = 329.63
-E_freq = 392.00
-C5_freq = 523.25
-
-# get timesteps for each sample, T is note duration in seconds
-sample_rate = 44100
-T = 0.75
-t = np.linspace(0, T, int(T * sample_rate), False)
-
-# generate sine wave notes
-A_note = np.sin(A_freq * t * 2 * np.pi)
-Csh_note = np.sin(Csh_freq * t * 2 * np.pi)
-E_note = np.sin(E_freq * t * 2 * np.pi)
-C5_note = np.sin(C5_freq * t * 2 * np.pi)
-
-# mix audio together
-audio = np.zeros((44100, 2))
-n = len(t)
-offset = 0
-audio[0 + offset: n + offset, 0] += A_note
-audio[0 + offset: n + offset, 1] += 0.125 * A_note
-#offset = 0 to overlap notes completely
-offset = 5500
-audio[0 + offset: n + offset, 0] += 0.5 * Csh_note
-audio[0 + offset: n + offset, 1] += 0.5 * Csh_note
-offset = 11000
-audio[0 + offset: n + offset, 0] += 0.125 * E_note
-audio[0 + offset: n + offset, 1] += E_note
-
-
-# normalize to 16-bit range
-audio *= 32767 / np.max(np.abs(audio))
-# convert to 16-bit data
-audio = audio.astype(np.int16)
-
-# start playback
-play_obj = sa.play_buffer(audio, 2, 2, sample_rate)
-
-#  wait for playback to finish before exiting
-play_obj.wait_done()
-
-audio = np.zeros((44100, 2))
-n = len(t)
-offset = 0
-audio[0 + offset: n + offset, 0] += E_note
-audio[0 + offset: n + offset, 1] += 0.125 * E_note
-#offset = 0 to overlap notes completely
-offset = 5500
-audio[0 + offset: n + offset, 0] += 0.5 * Csh_note
-audio[0 + offset: n + offset, 1] += 0.5 * Csh_note
-offset = 11000
-audio[0 + offset: n + offset, 0] += 0.125 * A_note
-audio[0 + offset: n + offset, 1] += A_note
-
-
-# normalize to 16-bit range
-audio *= 32767 / np.max(np.abs(audio))
-# convert to 16-bit data
-audio = audio.astype(np.int16)
-
-# start playback
-play_obj = sa.play_buffer(audio, 2, 2, sample_rate)
-
-# # wait for playback to finish before exiting
-play_obj.wait_done()
-
-colors = 'images/image.png'
-
-squidward = 'images/squidward.jpg'
-
-starry_night = 'images/starry_night.jpeg'
-
-image = Image.open(color_image, 'r')
-row_length, num_rows = image.size
-pix_val = list(image.getdata())
-#pix_val = [[0,0,0],[1,1,1],[2,2,2]]
-result = []
-rows = []
-#print(result)
-temp = 0
-for i in range(0, len(pix_val)):
-    if (i % row_length == 0):
-        val = int(temp / row_length)
-        rows.append(val)
-        temp = 0
-    else:
-        temp += np.average(pix_val[i])
-
-temp = 0
-for i in range(0, len(rows)):
-    if (i % 9 == 0):
-        val = int(temp / 9)
-        result.append(val)
-        temp = 0
-    else:
-        temp += rows[i]
-
-
-
-
-all_rows = result
-
-previous_val = -1
-curr_octave = 5
-row_index = 0
-up_or_down = 1
-while row_index < len(rows) - 1:
-    curr_val = rows[row_index]
-    if curr_val > previous_val:
-        if up_or_down == -1:
-            C_freq = NOTES["C" + str(curr_octave)]
-            E_freq = NOTES["E" + str()]
-            
-        if up_or_down == 1:
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-A_freq = 261.63
-Csh_freq = 329.63
-E_freq = 392.00
-C5_freq = 523.25
-
-# get timesteps for each sample, T is note duration in seconds
-sample_rate = 44100
-T = 0.75
-t = np.linspace(0, T, int(T * sample_rate), False)
-
-# generate sine wave notes
-A_note = np.sin(A_freq * t * 2 * np.pi)
-Csh_note = np.sin(Csh_freq * t * 2 * np.pi)
-E_note = np.sin(E_freq * t * 2 * np.pi)
-C5_note = np.sin(C5_freq * t * 2 * np.pi)
-
-# mix audio together
-audio = np.zeros((44100, 2))
-n = len(t)
-offset = 0
-audio[0 + offset: n + offset, 0] += A_note
-audio[0 + offset: n + offset, 1] += 0.125 * A_note
-#offset = 0 to overlap notes completely
-offset = 5500
-audio[0 + offset: n + offset, 0] += 0.5 * Csh_note
-audio[0 + offset: n + offset, 1] += 0.5 * Csh_note
-offset = 11000
-audio[0 + offset: n + offset, 0] += 0.125 * E_note
-audio[0 + offset: n + offset, 1] += E_note
-
-
-# normalize to 16-bit range
-audio *= 32767 / np.max(np.abs(audio))
-# convert to 16-bit data
-audio = audio.astype(np.int16)
-
-# start playback
-play_obj = sa.play_buffer(audio, 2, 2, sample_rate)
-
-#  wait for playback to finish before exiting
-play_obj.wait_done()
-
-audio = np.zeros((44100, 2))
-n = len(t)
-offset = 0
-audio[0 + offset: n + offset, 0] += E_note
-audio[0 + offset: n + offset, 1] += 0.125 * E_note
-#offset = 0 to overlap notes completely
-offset = 5500
-audio[0 + offset: n + offset, 0] += 0.5 * Csh_note
-audio[0 + offset: n + offset, 1] += 0.5 * Csh_note
-offset = 11000
-audio[0 + offset: n + offset, 0] += 0.125 * A_note
-audio[0 + offset: n + offset, 1] += A_note
-
-
-# normalize to 16-bit range
-audio *= 32767 / np.max(np.abs(audio))
-# convert to 16-bit data
-audio = audio.astype(np.int16)
-
-# start playback
-play_obj = sa.play_buffer(audio, 2, 2, sample_rate)
-
-# # wait for playback to finish before exiting
-play_obj.wait_done()
-j.gepj.thgin_rrats/segami'' =th thgin_yrrayrratsgpj.drawdiuqs/segami'' = drawdiuqsItsetgnpacneld oy-warrhol-n=ewm-/yosrk-arti
-est.'
-iogag
- imag''ea = Image.ompeins(color_image, 'r')
-row_length, num_rows = image.size
-pix_val = list(image.getdata())
-#pix_val = [[0,0,0],[1,1,1],[2,2,2]]
-result = []
-rows = []
-#print(result)
-temp = 0
-for i in range(0, len(pix_val)):
-    if (i % row_length == 0):
-        val = int(temp / row_length)
-        rows.append(val)
-        temp = 0
-    else:
-        temp += np.average(pix_val[i])
-
-temp = 0
-for i in range(0, len(rows)):
-    if (i % 9 == 0):
-        val = int(temp / 9)
-        result.append(val)
-        temp = 0
-    else:
-        temp += rows[i]
-
-
-
-
-all_rows = result
-
-previous_val = -1
-curr_octave = 5
-row_index = 0
-up_or_down = 1
-while row_index < len(rows) - 1:
-    curr_val = rows[row_index]
-    if curr_val > previous_val:
-        if up_or_down == -1:
-            C_freq = NOTES["C" + str(curr_octave)]
-            E_freq = NOTES["E" + str(curr_octave)]
-            G_freq = NOTES["G" + str(curr_octave)]
-        if up_or_down == 1:
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-A_freq = 261.63
-Csh_freq = 329.63
-E_freq = 392.00
-C5_freq = 523.25
-
-# get timesteps for each sample, T is note duration in seconds
-sample_rate = 44100
-T = 0.75
-t = np.linspace(0, T, int(T * sample_rate), False)
-
-# generate sine wave notes
-A_note = np.sin(A_freq * t * 2 * np.pi)
-Csh_note = np.sin(Csh_freq * t * 2 * np.pi)
-E_note = np.sin(E_freq * t * 2 * np.pi)
-C5_note = np.sin(C5_freq * t * 2 * np.pi)
-
-# mix audio together
-audio = np.zeros((44100, 2))
-n = len(t)
-offset = 0
-audio[0 + offset: n + offset, 0] += A_note
-audio[0 + offset: n + offset, 1] += 0.125 * A_note
-#offset = 0 to overlap notes completely
-offset = 5500
-audio[0 + offset: n + offset, 0] += 0.5 * Csh_note
-audio[0 + offset: n + offset, 1] += 0.5 * Csh_note
-offset = 11000
-audio[0 + offset: n + offset, 0] += 0.125 * E_note
-audio[0 + offset: n + offset, 1] += E_note
-
-
-# normalize to 16-bit range
-audio *= 32767 / np.max(np.abs(audio))
-# convert to 16-bit data
-audio = audio.astype(np.int16)
-
-# start playback
-play_obj = sa.play_buffer(audio, 2, 2, sample_rate)
-
-#  wait for playback to finish before exiting
-play_obj.wait_done()
-
-audio = np.zeros((44100, 2))
-n = len(t)
-offset = 0
-audio[0 + offset: n + offset, 0] += E_note
-audio[0 + offset: n + offset, 1] += 0.125 * E_note
-#offset = 0 to overlap notes completely
-offset = 5500
-audio[0 + offset: n + offset, 0] += 0.5 * Csh_note
-audio[0 + offset: n + offset, 1] += 0.5 * Csh_note
-offset = 11000
-audio[0 + offset: n + offset, 0] += 0.125 * A_note
-audio[0 + offset: n + offset, 1] += A_note
-
-
-# normalize to 16-bit range
-audio *= 32767 / np.max(np.abs(audio))
-# convert to 16-bit data
-audio = audio.astype(np.int16)
-
-# start playback
-play_obj = sa.play_buffer(audio, 2, 2, sample_rate)
-
-# # wait for playback to finish before exiting
-play_obj.wait_done()
+        # If the current row value is greater than the previous 
+        # row value, go down
+        elif curr_val < previous_val:
+
+            # If last "move" was a down, descend an octave
+            if up_or_down == -1:
+                if curr_octave == 2:
+                    play_notes_down(NOTES, curr_octave)
+                else:
+                    curr_octave -= 1
+                    play_notes_down(NOTES, curr_octave)
+        
+            # If last move was an up, descend, but in the same octave
+            elif up_or_down == 1:
+                play_notes_down(NOTES, curr_octave)
+                up_or_down = -1
+        
+        previous_val = curr_val
+        row_index += 1
+
+if __name__ == "__main__":
+    val = input("Enter the filename for the image you want to play: ")
+    playImage(val)
